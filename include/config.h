bd6eb1fd87a9298c43ed8e7431518fccddc14033
Arceveti <73617174+Arceveti@users.noreply.github.com>
2021-09-30 20:58:15 -0700

    Disable HD_SHADOWS by default

--- include/config.h
+++ include/config.h
@@ -183,7 +183,7 @@
 // Also, this also disables anti-aliasing on Mario.
 #define SILHOUETTE 127
 // Use 64x64 quarter shadow textures (Vanilla are 16x16)
-#define HD_SHADOWS
+//#define HD_SHADOWS
 // Makes certain objects (mainly trees) transparent when the camera gets close
 #define OBJ_OPACITY_BY_CAM_DIST
 // Fixes the game reading the ia8 burn smoke texture as an rgba16
