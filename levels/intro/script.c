551e495dd2ee6a9aa1929555014e9aa18e6e2325
gheskett <gheskett@gmail.com>
2021-10-07 23:22:49 -0400

    Call area_update_objects instead for proper animation loading

--- levels/intro/script.c
+++ levels/intro/script.c
@@ -54,12 +54,12 @@ const LevelScript level_intro_splash_screen[] = {
     // CALL_LOOP(/*arg*/ 1, /*func*/ lvl_init_or_update),
     
     JUMP_LINK_PUSH_ARG(75),
-        CALL(/*arg*/ 0, /*func*/ update_objects),
+        CALL(/*arg*/ 0, /*func*/ area_update_objects),
         SLEEP(/*frames*/ 1),
     JUMP_N_TIMES(),
     TRANSITION(/*transType*/ WARP_TRANSITION_FADE_INTO_COLOR, /*time*/ 16, /*color*/ 0x00, 0x00, 0x00),
     JUMP_LINK_PUSH_ARG(16),
-        CALL(/*arg*/ 0, /*func*/ update_objects),
+        CALL(/*arg*/ 0, /*func*/ area_update_objects),
         SLEEP(/*frames*/ 1),
     JUMP_N_TIMES(),
 #else
