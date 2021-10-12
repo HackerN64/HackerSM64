24a2905fc3559fc4dc11671f84382b718ceb1aa7
Arceveti <73617174+Arceveti@users.noreply.github.com>
2021-10-11 20:25:35 -0700

    Remove some unused functions

--- src/game/spawn_object.c
+++ src/game/spawn_object.c
@@ -15,64 +15,6 @@
 #include "puppylights.h"
 
 /**
- * An unused linked list struct that seems to have been replaced by ObjectNode.
- */
-struct LinkedList {
-    struct LinkedList *next;
-    struct LinkedList *prev;
-};
-
-/**
- * Clear the doubly linked usedList. Singly link each item in the pool into
- * a list, and return this list in pFreeList.
- * Appears to have been replaced by init_free_object_list.
- */
-void unused_init_free_list(struct LinkedList *usedList, struct LinkedList **pFreeList,
-                           struct LinkedList *pool, s32 itemSize, s32 poolLength) {
-    s32 i;
-    struct LinkedList *node = pool;
-
-    usedList->next = usedList;
-    usedList->prev = usedList;
-
-    *pFreeList = pool;
-
-    for (i = 0; i < poolLength - 1; i++) {
-        // Add next node to free list
-        node = (struct LinkedList *) ((u8 *) node + itemSize);
-        pool->next = node;
-        pool = node;
-    }
-
-    // End the list
-    pool->next = NULL;
-}
-
-/**
- * Attempt to allocate a node from freeList (singly linked) and append it
- * to the end of destList (doubly linked). Return the object, or NULL if
- * freeList is empty.
- * Appears to have been replaced by try_allocate_object.
- */
-struct LinkedList *unused_try_allocate(struct LinkedList *destList,
-                                       struct LinkedList *freeList) {
-    struct LinkedList *node = freeList->next;
-
-    if (node != NULL) {
-        // Remove from free list
-        freeList->next = node->next;
-
-        // Insert at the end of destination list
-        node->prev = destList->prev;
-        node->next = destList;
-        destList->prev->next = node;
-        destList->prev = node;
-    }
-
-    return node;
-}
-
-/**
  * Attempt to allocate an object from freeList (singly linked) and append it
  * to the end of destList (doubly linked). Return the object, or NULL if
  * freeList is empty.
@@ -100,20 +42,6 @@ struct Object *try_allocate_object(struct ObjectNode *destList, struct ObjectNod
 }
 
 /**
- * Remove the node from the doubly linked list it's in, and place it in the
- * singly linked freeList.
- * This function seems to have been replaced by deallocate_object.
- */
-void unused_deallocate(struct LinkedList *freeList, struct LinkedList *node) {
-    // Remove from doubly linked list
-    node->next->prev = node->prev;
-    node->prev->next = node->next;
-
-    // Insert at beginning of singly linked list
-    node->next = freeList->next;
-    freeList->next = node;
-}
-/**
  * Remove the given object from the object list that it's currently in, and
  * insert it at the beginning of the free list (singly linked).
  */
@@ -139,7 +67,7 @@ void init_free_object_list(void) {
     gFreeObjectList.next = (struct ObjectNode *) obj;
 
     // Link each object in the pool to the following object
-    for (i = 0; i < poolLength - 1; i++) {
+    for (i = 0; i < (poolLength - 1); i++) {
         obj->header.next = &(obj + 1)->header;
         obj++;
     }
@@ -157,29 +85,6 @@ void clear_object_lists(struct ObjectNode *objLists) {
     for (i = 0; i < NUM_OBJ_LISTS; i++) {
         objLists[i].next = &objLists[i];
         objLists[i].prev = &objLists[i];
-    }
-}
-
-/**
- * This function looks broken, but it appears to attempt to delete the leaf
- * graph nodes under obj and obj's siblings.
- */
-UNUSED static void unused_delete_leaf_nodes(struct Object *obj) {
-    struct Object *children;
-    struct Object *sibling;
-    struct Object *obj0 = obj;
-
-    if ((children = (struct Object *) obj->header.gfx.node.children) != NULL) {
-        unused_delete_leaf_nodes(children);
-    } else {
-        // No children
-        obj_mark_for_deletion(obj);
-    }
-
-    // Probably meant to be !=
-    while ((sibling = (struct Object *) obj->header.gfx.node.next) == obj0) {
-        unused_delete_leaf_nodes(sibling);
-        obj = (struct Object *) sibling->header.gfx.node.next;
     }
 }
 
@@ -237,7 +142,7 @@ struct Object *allocate_object(struct ObjectNode *objList) {
 
     // Initialize object fields
 
-    obj->activeFlags = ACTIVE_FLAG_ACTIVE | ACTIVE_FLAG_ALLOCATED;
+    obj->activeFlags = (ACTIVE_FLAG_ACTIVE | ACTIVE_FLAG_ALLOCATED);
     obj->parentObj = obj;
     obj->prevObj = NULL;
     obj->collidedObjInteractTypes = 0;
@@ -283,9 +188,7 @@ struct Object *allocate_object(struct ObjectNode *objList) {
 
     obj->header.gfx.node.flags &= ~GRAPH_RENDER_INVISIBLE;
     obj->header.gfx.node.flags |=  GRAPH_RENDER_UCODE_REJ;
-    obj->header.gfx.pos[0] = -10000.0f;
-    obj->header.gfx.pos[1] = -10000.0f;
-    obj->header.gfx.pos[2] = -10000.0f;
+    vec3_same(obj->header.gfx.pos, -10000.0f);
     obj->header.gfx.throwMatrix = NULL;
 #ifdef PUPPYLIGHTS
     obj->oLightID = 0xFFFF;
@@ -302,7 +205,7 @@ static void snap_object_to_floor(struct Object *obj) {
 
     obj->oFloorHeight = find_floor(obj->oPosX, obj->oPosY, obj->oPosZ, &surface);
 
-    if (obj->oFloorHeight + 2.0f > obj->oPosY && obj->oPosY > obj->oFloorHeight - 10.0f) {
+    if (((obj->oFloorHeight + 2.0f) > obj->oPosY) && (obj->oPosY > (obj->oFloorHeight - 10.0f))) {
         obj->oPosY = obj->oFloorHeight;
         obj->oMoveFlags |= OBJ_MOVE_ON_GROUND;
     }
@@ -320,7 +223,7 @@ struct Object *create_object(const BehaviorScript *bhvScript) {
     // If the first behavior script command is "begin <object list>", then
     // extract the object list from it
     if ((bhvScript[0] >> 24) == 0) {
-        objListIndex = (bhvScript[0] >> 16) & 0xFFFF;
+        objListIndex = ((bhvScript[0] >> 16) & 0xFFFF);
     } else {
         objListIndex = OBJ_LIST_DEFAULT;
     }
