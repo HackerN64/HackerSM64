90122a9408b29d8c6e4e5af0151134d21e4795b0
Arceveti <73617174+Arceveti@users.noreply.github.com>
2021-10-20 09:37:23 -0700

    Revert "COURSE_NUM_TO_INDEX Usage"
    This reverts commit ef22ecf730c338c057b426c641beb0f2465ab971.

--- src/game/behaviors/yoshi.inc.c
+++ src/game/behaviors/yoshi.inc.c
@@ -11,7 +11,8 @@ void bhv_yoshi_init(void) {
     o->oBuoyancy = 1.3f;
     o->oInteractionSubtype = INT_SUBTYPE_NPC;
 
-    if ((save_file_get_total_star_count((gCurrSaveFileNum - 1), COURSE_NUM_TO_INDEX(COURSE_MIN), COURSE_NUM_TO_INDEX(COURSE_MAX)) < 120) || sYoshiDead) {
+    if (save_file_get_total_star_count(gCurrSaveFileNum - 1, COURSE_MIN - 1, COURSE_MAX - 1) < 120
+        || sYoshiDead == TRUE) {
         o->activeFlags = ACTIVE_FLAG_DEACTIVATED;
     }
 }
