f00fdabc0ce5a309f82b2e2a7208a6a04b3d85ef
Arceveti <73617174+Arceveti@users.noreply.github.com>
2021-10-21 20:23:10 -0700

    Split config.h into multiple files

---
+++ include/config/config_menu.h
@@ -0,0 +1,31 @@
+#pragma once
+
+/*****************
+ * MENU SETTINGS *
+ *****************/
+
+// -- EXIT COURSE SETTINGS --
+
+// Disable exit course
+//#define DISABLE_EXIT_COURSE
+
+// Decides whether you can exit course while moving (has no effect if you disable exit course)
+#define EXIT_COURSE_WHILE_MOVING
+
+// Decides which level "exit course" takes you to (has no effect if you disable exit course)
+#define EXIT_COURSE_LEVEL LEVEL_CASTLE
+
+// Decides the area node "exit course" takes you to (has no effect if you disable exit course)
+#define EXIT_COURSE_AREA 0x01
+
+// Decides the warp node "exit course" takes you to (has no effect if you disable exit course)
+#define EXIT_COURSE_NODE 0x1F
+
+// -- Compatibility safeguards. Don't mess with these unless you know what you're doing. --
+
+#ifdef DISABLE_EXIT_COURSE
+#undef EXIT_COURSE_WHILE_MOVING
+#undef EXIT_COURSE_LEVEL
+#undef EXIT_COURSE_AREA
+#undef EXIT_COURSE_NODE
+#endif // DISABLE_EXIT_COURSE
