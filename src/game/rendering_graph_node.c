cad29520ffc0304a328ff4e98fead89f5e47e80d
Arceveti <73617174+Arceveti@users.noreply.github.com>
2021-11-01 16:29:07 -0700

    New compiler flags experiment

--- src/game/rendering_graph_node.c
+++ src/game/rendering_graph_node.c
@@ -372,14 +372,14 @@ void geo_append_display_list(void *displayList, s32 layer) {
     }
 }
 
-inline void inc_mat_stack() {
+static inline void inc_mat_stack() {
     Mtx *mtx = alloc_display_list(sizeof(*mtx));
     gMatStackIndex++;
     mtxf_to_mtx(mtx, gMatStack[gMatStackIndex]);
     gMatStackFixed[gMatStackIndex] = mtx;
 }
 
-inline void append_dl_and_return(struct GraphNodeDisplayList *node) {
+static inline void append_dl_and_return(struct GraphNodeDisplayList *node) {
     if (node->displayList != NULL) {
         geo_append_display_list(node->displayList, GET_GRAPH_NODE_LAYER(node->node.flags));
     }
