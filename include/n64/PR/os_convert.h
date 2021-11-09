54844e0a41af5ede81ec1105e341625ee77890e6
Arceveti <73617174+Arceveti@users.noreply.github.com>
2021-11-09 12:43:48 -0800

    os_convert.h formatting

--- include/n64/PR/os_convert.h
+++ include/n64/PR/os_convert.h
@@ -54,7 +54,7 @@ extern "C" {
  */
 
 #define	OS_CLOCK_RATE		62500000LL
-#define	OS_CPU_COUNTER		(OS_CLOCK_RATE*3/4)
+#define	OS_CPU_COUNTER		(OS_CLOCK_RATE * 3 / 4) // 46875000
 
 
 #if defined(_LANGUAGE_C) || defined(_LANGUAGE_C_PLUS_PLUS)
@@ -65,22 +65,22 @@ extern "C" {
  *
  */
 
-#define OS_NSEC_TO_CYCLES(n)	(((u64)(n)*(OS_CPU_COUNTER/15625000LL))/(1000000000LL/15625000LL))
-#define OS_USEC_TO_CYCLES(n)	(((u64)(n)*(OS_CPU_COUNTER/15625LL))/(1000000LL/15625LL))
-#define OS_CYCLES_TO_NSEC(c)	(((u64)(c)*(1000000000LL/15625000LL))/(OS_CPU_COUNTER/15625000LL))
-#define OS_CYCLES_TO_USEC(c)	(((u64)(c)*(1000000LL/15625LL))/(OS_CPU_COUNTER/15625LL))
+#define OS_NSEC_TO_CYCLES(n)	(((u64)(n) * (OS_CPU_COUNTER / 15625000LL)) / (  1000000000LL / 15625000LL)) // ((n *    3) /   64)
+#define OS_USEC_TO_CYCLES(n)	(((u64)(n) * (OS_CPU_COUNTER /    15625LL)) / (     1000000LL /    15625LL)) // ((n * 3000) /   64)
+#define OS_CYCLES_TO_NSEC(c)	(((u64)(c) * (  1000000000LL / 15625000LL)) / (OS_CPU_COUNTER / 15625000LL)) // ((c *   64) /    3)
+#define OS_CYCLES_TO_USEC(c)	(((u64)(c) * (     1000000LL /    15625LL)) / (OS_CPU_COUNTER /    15625LL)) // ((c *   64) / 3000)
 
 /* OS_K?_TO_PHYSICAL macro bug fix for CodeWarrior */
 #ifndef __MWERKS__
-#define	OS_K0_TO_PHYSICAL(x)	(u32)(((char *)(x)-0x80000000))
-#define	OS_K1_TO_PHYSICAL(x)	(u32)(((char *)(x)-0xa0000000))
+#define	OS_K0_TO_PHYSICAL(x)	(u32)(((char *)(x) - 0x80000000))
+#define	OS_K1_TO_PHYSICAL(x)	(u32)(((char *)(x) - 0xa0000000))
 #else
-#define	OS_K0_TO_PHYSICAL(x)	((char *)(x)-0x80000000)
-#define	OS_K1_TO_PHYSICAL(x)	((char *)(x)-0xa0000000)
+#define	OS_K0_TO_PHYSICAL(x)	      ((char *)(x) - 0x80000000)
+#define	OS_K1_TO_PHYSICAL(x)	      ((char *)(x) - 0xa0000000)
 #endif
 
-#define	OS_PHYSICAL_TO_K0(x)	(void *)(((u32)(x)+0x80000000))
-#define	OS_PHYSICAL_TO_K1(x)	(void *)(((u32)(x)+0xa0000000))
+#define	OS_PHYSICAL_TO_K0(x)	(void *)(((u32)(x) + 0x80000000))
+#define	OS_PHYSICAL_TO_K1(x)	(void *)(((u32)(x) + 0xa0000000))
 
 
 /**************************************************************************
@@ -98,8 +98,8 @@ extern "C" {
 
 /* Address translation routines and macros */
 
-extern u32		 osVirtualToPhysical(void *vaddr);
-extern void *		 osPhysicalToVirtual(u32   paddr);
+extern u32   osVirtualToPhysical(void *vaddr);
+extern void *osPhysicalToVirtual(u32   paddr);
 
 
 #endif  /* defined(_LANGUAGE_C) || defined(_LANGUAGE_C_PLUS_PLUS) */
