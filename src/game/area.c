d7e0aff39c83561c2f97e65e8a420a0d298cf8e2
Arceveti <73617174+Arceveti@users.noreply.github.com>
2021-11-08 18:23:22 -0800

    re-implpement puppyprint graph profiling "fix"

--- src/game/area.c
+++ src/game/area.c
@@ -430,7 +430,7 @@ void render_game(void) {
 #if PUPPYPRINT_DEBUG
     profiler_update(graphTime, first);
     graphTime[perfIteration] -= (collisionTime[perfIteration] - colTime);
-    graphTime[perfIteration] -=   profilerTime[perfIteration]; //! Graph time is inaccurate and wrongly reaches 0 sometimes
+    // graphTime[perfIteration] -=   profilerTime[perfIteration]; //! Graph time is inaccurate and wrongly reaches 0 sometimes
 #endif
 #if PUPPYPRINT_DEBUG
     puppyprint_render_profiler();
