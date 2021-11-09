d2d258a7ca9056150641ebac73187febbe7d6d3a
Arceveti <73617174+Arceveti@users.noreply.github.com>
2021-11-08 20:57:33 -0800

    Disable OBJECTS_REJ by default

--- include/config/config_graphics.h
+++ include/config/config_graphics.h
@@ -23,8 +23,8 @@
 // Disable AA (Improves console performance but causes visible seams between unconnected geometry).
 #define DISABLE_AA
 
-// Use .rej microcode for certain objects.
-#define OBJECTS_REJ
+// Use .rej microcode for certain objects (experimental).
+//#define OBJECTS_REJ
 
 // Makes the coins ia8 64x64 instead of ia16 32x32. Uses new ia8 textures so that vanilla coins look better.
 #define IA8_COINS
