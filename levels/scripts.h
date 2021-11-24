b321c003c341c93fbdf976d3ba28b6e107c698dd
Arceveti <73617174+Arceveti@users.noreply.github.com>
2021-11-23 19:12:58 -0800

    Revert global level script renames + add defines so level command presets still work

--- levels/scripts.h
+++ levels/scripts.h
@@ -6,23 +6,42 @@
 
 // scripts
 extern const LevelScript level_main_scripts_entry[];
-extern const LevelScript script_func_global_common0[];
-extern const LevelScript script_func_global_group1[];
-extern const LevelScript script_func_global_group2[];
-extern const LevelScript script_func_global_group3[];
-extern const LevelScript script_func_global_group4[];
-extern const LevelScript script_func_global_group5[];
-extern const LevelScript script_func_global_group6[];
-extern const LevelScript script_func_global_group7[];
-extern const LevelScript script_func_global_group8[];
-extern const LevelScript script_func_global_group9[];
-extern const LevelScript script_func_global_group10[];
-extern const LevelScript script_func_global_group11[];
-extern const LevelScript script_func_global_group12[];
-extern const LevelScript script_func_global_group13[];
-extern const LevelScript script_func_global_group14[];
-extern const LevelScript script_func_global_group15[];
-extern const LevelScript script_func_global_group16[];
-extern const LevelScript script_func_global_group17[];
+extern const LevelScript script_func_global_1[];
+extern const LevelScript script_func_global_2[];
+extern const LevelScript script_func_global_3[];
+extern const LevelScript script_func_global_4[];
+extern const LevelScript script_func_global_5[];
+extern const LevelScript script_func_global_6[];
+extern const LevelScript script_func_global_7[];
+extern const LevelScript script_func_global_8[];
+extern const LevelScript script_func_global_9[];
+extern const LevelScript script_func_global_10[];
+extern const LevelScript script_func_global_11[];
+extern const LevelScript script_func_global_12[];
+extern const LevelScript script_func_global_13[];
+extern const LevelScript script_func_global_14[];
+extern const LevelScript script_func_global_15[];
+extern const LevelScript script_func_global_16[];
+extern const LevelScript script_func_global_17[];
+extern const LevelScript script_func_global_18[];
+
+#define  script_func_global_common0 script_func_global_1
+#define  script_func_global_group1  script_func_global_2
+#define  script_func_global_group2  script_func_global_3
+#define  script_func_global_group3  script_func_global_4
+#define  script_func_global_group4  script_func_global_5
+#define  script_func_global_group5  script_func_global_6
+#define  script_func_global_group6  script_func_global_7
+#define  script_func_global_group7  script_func_global_8
+#define  script_func_global_group8  script_func_global_9
+#define  script_func_global_group9  script_func_global_10
+#define  script_func_global_group10 script_func_global_11
+#define  script_func_global_group11 script_func_global_12
+#define  script_func_global_group12 script_func_global_13
+#define  script_func_global_group13 script_func_global_14
+#define  script_func_global_group14 script_func_global_15
+#define  script_func_global_group15 script_func_global_16
+#define  script_func_global_group16 script_func_global_17
+#define  script_func_global_group17 script_func_global_18
 
 #endif
