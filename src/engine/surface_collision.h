b529941cbb2d359667e93b18a616b393a3a88531
thecozies <collinpferguson@gmail.com>
2021-12-11 15:55:42 -0600

    surface collision performance improvements

--- src/engine/surface_collision.h
+++ src/engine/surface_collision.h
@@ -7,23 +7,29 @@
 
 #include "engine/extended_bounds.h"
 
-#define CELL_HEIGHT_LIMIT           20000
+#define FIND_FLOOR_BUFFER               78
+
+#define CELL_HEIGHT_LIMIT            20000
 #define FLOOR_LOWER_LIMIT           -11000
 #define FLOOR_LOWER_LIMIT_MISC      (FLOOR_LOWER_LIMIT + 1000)
-// same as FLOOR_LOWER_LIMIT_MISC, explicitly for shadow.c
-#define FLOOR_LOWER_LIMIT_SHADOW    (FLOOR_LOWER_LIMIT + 1000)
 
-#define is_outside_level_bounds(xPos, zPos) (((xPos) <= -LEVEL_BOUNDARY_MAX) || ((xPos) >=  LEVEL_BOUNDARY_MAX) || ((zPos) <= -LEVEL_BOUNDARY_MAX) || ((zPos) >=  LEVEL_BOUNDARY_MAX))
+#define is_outside_level_bounds(xPos, zPos) \
+    (((xPos) <= -LEVEL_BOUNDARY_MAX) ||     \
+     ((xPos) >=  LEVEL_BOUNDARY_MAX) ||     \
+     ((zPos) <= -LEVEL_BOUNDARY_MAX) ||     \
+     ((zPos) >=  LEVEL_BOUNDARY_MAX))
 
-#define get_surface_height_at_location(xPos, zPos, surf) (-((xPos) * (surf)->normal.x + (surf)->normal.z * (zPos) + (surf)->originOffset) / (surf)->normal.y)
+#define get_surface_height_at_location(xPos, zPos, surf) (-(((xPos) * (surf)->normal.x) + ((zPos) * (surf)->normal.z) + (surf)->originOffset) / (surf)->normal.y)
 
 #define SURFACE_YAW(s) (atan2s(((s)->normal.z), ((s)->normal.x)))
 
-#define RAYCAST_FIND_FLOOR  (1 << 0)
-#define RAYCAST_FIND_WALL   (1 << 1)
-#define RAYCAST_FIND_CEIL   (1 << 2)
-#define RAYCAST_FIND_WATER  (1 << 3)
-#define RAYCAST_FIND_ALL    (0xFFFFFFFF)
+enum RaycastFlags {
+    RAYCAST_FIND_FLOOR = (1 << 0),
+    RAYCAST_FIND_WALL  = (1 << 1),
+    RAYCAST_FIND_CEIL  = (1 << 2),
+    RAYCAST_FIND_WATER = (1 << 3),
+    RAYCAST_FIND_ALL   = (0xFFFFFFFF)
+};
 
 struct WallCollisionData {
     /*0x00*/ f32 x, y, z;
@@ -44,6 +50,8 @@ f32 find_room_floor(f32 x, f32 y, f32 z, struct Surface **pfloor);
 s32 find_water_level_and_floor(s32 x, s32 z, struct Surface **pfloor);
 s32 find_water_level(s32 x, s32 z);
 s32 find_poison_gas_level(s32 x, s32 z);
+#ifdef VANILLA_DEBUG
 void debug_surface_list_info(f32 xPos, f32 zPos);
+#endif
 
 #endif // SURFACE_COLLISION_H
