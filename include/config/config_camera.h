81f642aba497de4d997a7b730bdeb12329e0876b
thecozies <collinpferguson@gmail.com>
2021-12-11 15:58:46 -0600

    camera conf documentation

--- include/config/config_camera.h
+++ include/config/config_camera.h
@@ -5,18 +5,24 @@
  *******************/
 
 // Force the camera mode to your choice (except when swimming or flying)
-//      Note: this is very aggressive (e.g. removes door cutscenes)
+// It does this by setting the area's camera->defMode to this mode, and also
+// changes hardcoded modes to use c->defMode
+//      Note: removes door cutscenes due to the way they're designed to work with specific modes
 //      Search for FORCED_CAMERA_MODE in camera.c for more information
 #define FORCED_CAMERA_MODE        CAMERA_MODE_8_DIRECTIONS
 
-/** Movement specific camera modes: **/
+// Changes hardcoded camera mode reverting to instead use the area's default mode (defMode)
+// If you're using a FORCED_CAMERA_MODE, this must be on for it to work
+#define USE_COURSE_DEFAULT_MODE
+
+/***** Movement specific camera modes: *****/
 // Camera mode when Mario is < 400 units away from the water surface (default is CAMERA_MODE_WATER_SURFACE)
 #define WATER_SURFACE_CAMERA_MODE CAMERA_MODE_WATER_SURFACE
 // Camera mode when Mario is > 800 units away from the water surface (default is CAMERA_MODE_BEHIND_MARIO)
 #define DEEP_WATER_CAMERA_MODE    CAMERA_MODE_BEHIND_MARIO
 // Camera mode when Mario is flying (default is CAMERA_MODE_BEHIND_MARIO)
 #define FLYING_CAMERA_MODE        CAMERA_MODE_BEHIND_MARIO
-/*************************************/
+/*******************************************/
 
 // Makes the camera approach Mario's height much more quickly
 #define FAST_VERTICAL_CAMERA_MOVEMENT
@@ -48,6 +54,9 @@
 /****** Compatibility safeguards ******/
 /**************************************/
 // Don't change these unless you know what you're doing
+#ifdef FORCED_CAMERA_MODE
+#define USE_COURSE_DEFAULT_MODE // Forced camera mode overwrites the default mode
+#endif
 #ifndef WATER_SURFACE_CAMERA_MODE
 #define WATER_SURFACE_CAMERA_MODE CAMERA_MODE_WATER_SURFACE
 #endif
