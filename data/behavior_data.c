eb0bb591f0484973409089397d6f6632ab1afed7
Arceveti <73617174+Arceveti@users.noreply.github.com>
2021-12-19 12:02:46 -0800

    Update data/behavior_data.c
    Co-authored-by: thecozies <79979276+thecozies@users.noreply.github.com>

--- data/behavior_data.c
+++ data/behavior_data.c
@@ -2662,7 +2662,7 @@ const BehaviorScript bhvBowserSubDoor[] = {
     BEGIN(OBJ_LIST_SURFACE),
     OR_LONG(oFlags, (OBJ_FLAG_ACTIVE_FROM_AFAR | OBJ_FLAG_UPDATE_GFX_POS_AND_ANGLE | OBJ_FLAG_UCODE_LARGE)),
     LOAD_COLLISION_DATA(ddd_seg7_collision_bowser_sub_door),
-    SET_FLOAT(oDrawingDistance,   20000),
+    SET_FLOAT(oDrawingDistance, 20000),
     SET_FLOAT(oCollisionDistance, 20000),
     BEGIN_LOOP(),
         CALL_NATIVE(bhv_bowsers_sub_loop),
