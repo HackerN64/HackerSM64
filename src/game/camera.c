4ef1d81a2f6d34bc851bd30f8dd6a419d9675d6f
thecozies <collinpferguson@gmail.com>
2021-12-23 07:18:11 -0600

    reonucam: reset 8dir base yaw to 0 when setting to 8dir mode

--- src/game/camera.c
+++ src/game/camera.c
@@ -2846,6 +2846,9 @@ void set_camera_mode(struct Camera *c, s16 mode, s16 frames) {
 #ifndef ENABLE_VANILLA_CAM_PROCESSING
         if (mode == CAMERA_MODE_8_DIRECTIONS) {
             // Helps transition from any camera mode to 8dir
+#ifdef REONUCAM
+            s8DirModeBaseYaw = 0;
+#endif
             s8DirModeYawOffset = snap_to_45_degrees(c->yaw);
         }
 #endif
