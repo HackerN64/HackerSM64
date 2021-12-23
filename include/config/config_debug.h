30a75bb1d89af3435a89d8be15a1e5bbe5977458
Reonu <danileon95@gmail.com>
2021-12-23 19:50:53 +0000

    fuck it *uncompletes your savefile*

--- include/config/config_debug.h
+++ include/config/config_debug.h
@@ -45,7 +45,7 @@
 
 // Same as above, but also reads all save file flags as complete.
 // This will not overwrite existing save file data unless you save over it.
-#define COMPLETE_SAVE_FILE
+//#define COMPLETE_SAVE_FILE
 
 // Removes the limit on FPS
 //#define UNLOCK_FPS
