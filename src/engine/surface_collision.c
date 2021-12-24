d1e9c84326f5234d3117f0d384b54b3661905b22
Arceveti <73617174+Arceveti@users.noreply.github.com>
2021-12-24 09:08:13 -0800

    Fix how shadows handle water/transparency/height (#228)
    * Fix how shadows handle water/transparency/height
    
    * Change shadow check for flying carpets from a level specific check to a oPlatformOnTrackType check
    
    Fixes #179

--- src/engine/surface_collision.c
+++ src/engine/surface_collision.c
@@ -650,7 +650,7 @@ f32 find_water_floor(s32 xPos, s32 yPos, s32 zPos, struct Surface **pfloor) {
 /**
  * Finds the height of water at a given location.
  */
-s32 find_water_level_and_floor(s32 x, s32 z, struct Surface **pfloor) {
+s32 find_water_level_and_floor(s32 x, s32 y, s32 z, struct Surface **pfloor) {
     s32 val;
     s32 loX, hiX, loZ, hiZ;
     TerrainData *p = gEnvironmentRegions;
@@ -658,7 +658,7 @@ s32 find_water_level_and_floor(s32 x, s32 z, struct Surface **pfloor) {
 #if PUPPYPRINT_DEBUG
     OSTime first = osGetTime();
 #endif
-    s32 waterLevel = find_water_floor(x, ((gCollisionFlags & COLLISION_FLAG_CAMERA) ? gLakituState.pos[1] : gMarioState->pos[1]), z, &floor);
+    s32 waterLevel = find_water_floor(x, y, z, &floor);
 
     if (p != NULL && waterLevel == FLOOR_LOWER_LIMIT) {
         s32 numRegions = *p++;
