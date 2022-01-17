52600eb453a72ec03a9a9fafc056419ea4a8bd25
Mr-Wiseguy <mrwiseguyromhacking@gmail.com>
2022-01-16 23:31:13 -0500

    Added RSP profiling to the lightweight profiler

--- src/game/profiling.c
+++ src/game/profiling.c
@@ -14,15 +14,18 @@ typedef struct {
 ProfileTimeData all_profiling_data[PROFILER_TIME_COUNT];
 
 int profile_buffer_index = -1;
+int rsp_buffer_indices[PROFILER_RSP_COUNT];
+// Holds either the start time if the task is running, or the amount of time the task has run for so far if yielded
+u32 rsp_pending_times[PROFILER_RSP_COUNT];
 u32 prev_start;
 u32 start;
 u32 prev_time;
 
-static void buffer_update(ProfileTimeData* data, u32 new) {
-    u32 old = data->counts[profile_buffer_index];
+static void buffer_update(ProfileTimeData* data, u32 new, int buffer_index) {
+    u32 old = data->counts[buffer_index];
     data->total -= old;
     data->total += new;
-    data->counts[profile_buffer_index] = new;
+    data->counts[buffer_index] = new;
 }
 
 void fast_profiler_update(enum ProfilerTime which) {
@@ -31,14 +34,41 @@ void fast_profiler_update(enum ProfilerTime which) {
     ProfileTimeData* cur_data = &all_profiling_data[which];
 
     diff = cur_time - prev_time;    
-    buffer_update(cur_data, diff);
+    buffer_update(cur_data, diff, profile_buffer_index);
     prev_time = cur_time;
 }
+
+void fast_profiler_rsp_started(enum ProfilerRSPTime which) {
+    rsp_pending_times[which] = osGetCount();
+}
+
+void fast_profiler_rsp_completed(enum ProfilerRSPTime which) {
+    ProfileTimeData* cur_data = &all_profiling_data[PROFILER_TIME_RSP_GFX + which];
+    int cur_index = rsp_buffer_indices[which];
+    u32 time = osGetCount() - rsp_pending_times[which];
+    rsp_pending_times[which] = 0;
+
+    buffer_update(cur_data, time, cur_index);
+    cur_index++;
+    if (cur_index >= PROFILING_BUFFER_SIZE) {
+        cur_index = 0;
+    }
+    rsp_buffer_indices[which] = cur_index;
+}
+
+void fast_profiler_rsp_resumed() {
+    rsp_pending_times[PROFILER_RSP_GFX] = osGetCount() - rsp_pending_times[PROFILER_RSP_GFX];
+}
+
+// This ends up being the same math as resumed, so we just use resumed for both
+// void fast_profiler_rsp_yielded() {
+//     rsp_pending_times[PROFILER_RSP_GFX] = osGetCount() - rsp_pending_times[PROFILER_RSP_GFX];
+// }
 
 static void update_fps_timer() {
     u32 diff = start - prev_start;
 
-    buffer_update(&all_profiling_data[PROFILER_TIME_FPS], diff);
+    buffer_update(&all_profiling_data[PROFILER_TIME_FPS], diff, profile_buffer_index);
     prev_start = start;
 }
 
@@ -56,14 +86,14 @@ static void update_rdp_timers() {
         IO_WRITE(DPC_STATUS_REG, (DPC_CLR_CLOCK_CTR | DPC_CLR_CMD_CTR | DPC_CLR_PIPE_CTR | DPC_CLR_TMEM_CTR));
     }
 
-    buffer_update(&all_profiling_data[PROFILER_TIME_TMEM], tmem);
-    buffer_update(&all_profiling_data[PROFILER_TIME_CMD], cmd);
-    buffer_update(&all_profiling_data[PROFILER_TIME_PIPE], pipe);
+    buffer_update(&all_profiling_data[PROFILER_TIME_TMEM], tmem, profile_buffer_index);
+    buffer_update(&all_profiling_data[PROFILER_TIME_CMD], cmd, profile_buffer_index);
+    buffer_update(&all_profiling_data[PROFILER_TIME_PIPE], pipe, profile_buffer_index);
 }
 
 void fast_profiler_print_times() {
     u32 microseconds[PROFILER_TIME_COUNT];
-    char text_buffer[128];
+    char text_buffer[196];
 
     update_fps_timer();
     update_total_timer();
@@ -89,7 +119,11 @@ void fast_profiler_print_times() {
         "RDP\n"
         "TMEM: %7d\n"
         "CMD:  %7d\n"
-        "PIPE: %7d",
+        "PIPE: %7d\n"
+        "RSP\n"
+        "GFX:  %7d\n"
+        "AUDIO:%7d\n"
+        "TOTAL:%7d",
         1000000.0f / microseconds[PROFILER_TIME_FPS],
         microseconds[PROFILER_TIME_CONTROLLERS],
         microseconds[PROFILER_TIME_LEVEL_SCRIPT],
@@ -99,7 +133,10 @@ void fast_profiler_print_times() {
         microseconds[PROFILER_TIME_TOTAL],
         microseconds[PROFILER_TIME_TMEM],
         microseconds[PROFILER_TIME_CMD],
-        microseconds[PROFILER_TIME_PIPE]
+        microseconds[PROFILER_TIME_PIPE],
+        microseconds[PROFILER_TIME_RSP_GFX],
+        microseconds[PROFILER_TIME_RSP_AUDIO],
+        microseconds[PROFILER_TIME_RSP_GFX] + microseconds[PROFILER_TIME_RSP_AUDIO]
     );
 
     Gfx* dlHead = gDisplayListHead;
--- src/game/profiling.h
+++ src/game/profiling.h
@@ -1,6 +1,9 @@
 #ifndef __PROFILING_H__
 #define __PROFILING_H__
 
+#include <ultra64.h>
+#include "macros.h"
+
 #define PROFILING_BUFFER_SIZE 64
 
 enum ProfilerTime {
@@ -14,11 +17,26 @@ enum ProfilerTime {
     PROFILER_TIME_TMEM,
     PROFILER_TIME_PIPE,
     PROFILER_TIME_CMD,
+    PROFILER_TIME_RSP_GFX,
+    PROFILER_TIME_RSP_AUDIO,
     PROFILER_TIME_COUNT,
+};
+
+enum ProfilerRSPTime {
+    PROFILER_RSP_GFX,
+    PROFILER_RSP_AUDIO,
+    PROFILER_RSP_COUNT
 };
 
 void fast_profiler_update(enum ProfilerTime which);
 void fast_profiler_print_times();
 void fast_profiler_frame_setup();
+void fast_profiler_rsp_started(enum ProfilerRSPTime which);
+void fast_profiler_rsp_completed(enum ProfilerRSPTime which);
+void fast_profiler_rsp_resumed();
+// See profiling.c to see why fast_profiler_rsp_yielded isn't its own function
+static ALWAYS_INLINE void fast_profiler_rsp_yielded() {
+    fast_profiler_rsp_resumed();
+}
 
 #endif
