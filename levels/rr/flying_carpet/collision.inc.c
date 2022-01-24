e5f670cafa4bc17226cf91c2ef525e0802d7937b
Arceveti <arceveti@gmail.com>
2022-01-24 11:04:13 -0800

    Fix rectangle

--- levels/rr/flying_carpet/collision.inc.c
+++ levels/rr/flying_carpet/collision.inc.c
@@ -2,13 +2,13 @@
 const Collision rr_seg7_collision_flying_carpet[] = {
     COL_INIT(),
     COL_VERTEX_INIT(0x4),
-    COL_VERTEX(-307, 20,  205),
-    COL_VERTEX(-307, 20, -205),
     COL_VERTEX( 307, 20, -205),
+    COL_VERTEX(-307, 20, -205),
+    COL_VERTEX(-307, 20,  205),
     COL_VERTEX( 307, 20,  205),
     COL_TRI_INIT(SURFACE_NOT_SLIPPERY, 2),
-    COL_TRI(0, 2, 1),
-    COL_TRI(1, 3, 2),
+    COL_TRI(0, 1, 2),
+    COL_TRI(0, 2, 3),
     COL_TRI_STOP(),
     COL_END(),
 };
