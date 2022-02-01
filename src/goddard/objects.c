2d03572d3c85114186599bf188aa37b074f38a4e
Arceveti <arceveti@gmail.com>
2022-01-31 20:12:42 -0800

    Remove goddard UB ifdefs

--- src/goddard/objects.c
+++ src/goddard/objects.c
@@ -347,10 +347,7 @@ struct ObjZone *make_zone(struct ObjGroup *a0, struct GdBoundingBox *bbox, struc
     newZone->unk2C = a2;
     newZone->unk30 = a0;
 
-//! @bug Created `ObjZone` is not returned
-#ifdef AVOID_UB
     return newZone;
-#endif
 }
 
 /* @ 22AF70 for 0x60 */
@@ -942,10 +939,7 @@ struct GdObj *func_8017E2F0(struct GdObj *obj, enum ObjTypeFlag type) {
         return obj;
     }
 
-//! @bug Nothing is returned if a GdObj of `type` is not found
-#ifdef AVOID_UB
     return NULL;
-#endif
 }
 
 /**
@@ -960,18 +954,10 @@ s32 apply_to_obj_types_in_group(s32 types, applyproc_t func, struct ObjGroup *gr
     enum ObjTypeFlag linkedObjType;
     applyproc_t objFn;
     UNUSED u8 filler[32];
-    s32 fnAppliedCount;
+    s32 fnAppliedCount = 0;
 
-    fnAppliedCount = 0;
-
-    //! @bug When `group` pointer is NULL, garbage is returned, not the
-    //!      count of `fn` calls
     if (group == NULL) {
-#ifdef AVOID_UB
         return fnAppliedCount;
-#else
-        return;
-#endif
     }
 
     if (group->linkType & 1) { // compressed data, not an Obj
