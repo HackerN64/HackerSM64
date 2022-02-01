ed0016c7f4fe49a7cd1b711f5eb3866b7971ea8c
Arceveti <arceveti@gmail.com>
2022-01-31 19:08:12 -0800

    Enable KEEP_MARIO_HEAD by default

--- include/config/config_goddard.h
+++ include/config/config_goddard.h
@@ -5,7 +5,7 @@
  ********************/
 
 // Enables Goddard (the Mario head).
-// #define KEEP_MARIO_HEAD
+#define KEEP_MARIO_HEAD
 
 // Enables the Goddard easter egg from Shindou (has no effect if KEEP_MARIO_HEAD is disabled).
 #define GODDARD_EASTER_EGG
