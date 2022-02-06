2cd7f0b59d21ca8279782d391da08ced1de2681c
Arceveti <arceveti@gmail.com>
2022-02-06 11:55:43 -0800

    Re-add comment about clip ratio in init_rsp

--- src/game/game_init.c
+++ src/game/game_init.c
@@ -129,11 +129,18 @@ const Gfx init_rdp[] = {
  */
 const Gfx init_rsp[] = {
     gsDPPipeSync(),
+
     gsSPClearGeometryMode(G_CULL_FRONT | G_FOG | G_LIGHTING | G_TEXTURE_GEN | G_TEXTURE_GEN_LINEAR | G_LOD),
     gsSPSetGeometryMode(G_SHADE | G_SHADING_SMOOTH | G_CULL_BACK | G_LIGHTING),
+
     gsSPNumLights(NUMLIGHTS_1),
+
     gsSPTexture(0, 0, 0, G_TX_RENDERTILE, G_OFF),
+
+    // Failing to set the clip ratio will result in warped triangles in F3DEX2:
+    // https://jrra.zone/n64/doc/n64man/gsp/gSPClipRatio.htm
     gsSPClipRatio(FRUSTRATIO_1),
+
     gsSPEndDisplayList(),
 };
 
