aa04129fd2664fe8dca3ce6b17e13f0238ab03e2
Mr-Wiseguy <mrwiseguyromhacking@gmail.com>
2022-02-14 17:01:42 -0500

    Ran script to convert all light commands to lightcolor commands

--- src/game/game_init.c
+++ src/game/game_init.c
@@ -127,6 +127,10 @@ const Gfx init_rdp[] = {
     gsSPEndDisplayList(),
 };
 
+Lights1 defaultLight = gdSPDefLights1(
+    0x3F, 0x3F, 0x3F, 0xFF, 0xFF, 0xFF, 0x28, 0x28, 0x28
+);
+
 /**
  * Sets the initial RSP (Reality Signal Processor) settings.
  */
@@ -134,7 +138,7 @@ const Gfx init_rsp[] = {
     gsDPPipeSync(),
     gsSPClearGeometryMode(G_CULL_FRONT | G_FOG | G_LIGHTING | G_TEXTURE_GEN | G_TEXTURE_GEN_LINEAR | G_LOD),
     gsSPSetGeometryMode(G_SHADE | G_SHADING_SMOOTH | G_CULL_BACK | G_LIGHTING),
-    gsSPNumLights(NUMLIGHTS_1),
+    gsSPSetLights1(defaultLight),
     gsSPTexture(0, 0, 0, G_TX_RENDERTILE, G_OFF),
     // @bug Failing to set the clip ratio will result in warped triangles in F3DEX2
     // without this change: https://jrra.zone/n64/doc/n64man/gsp/gSPClipRatio.htm
