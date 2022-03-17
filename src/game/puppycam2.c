f46622c197b6c4fc74c26c87d7a0be41cc945f95
Fazana <52551480+FazanaJ@users.noreply.github.com>
2022-03-17 15:56:10 +0000

    Update puppycam2.c

--- src/game/puppycam2.c
+++ src/game/puppycam2.c
@@ -1344,6 +1344,10 @@ void puppycam_loop(void) {
     if (!gPuppyCam.cutscene && sDelayedWarpOp == 0) {
         // Sets this before going through any possible modifications.
         gPuppyCam.flags = gPuppyCam.intendedFlags;
+#if PUPPYPRINT_DEBUG
+        if (sPPDebugPage == 7)
+            gPuppyCam.flags |= PUPPYCAM_BEHAVIOUR_FREE | PUPPYCAM_BEHAVIOUR_PITCH_ROTATION | PUPPYCAM_BEHAVIOUR_YAW_ROTATION;
+#endif
         puppycam_input_core();
         puppycam_projection();
         puppycam_script();
