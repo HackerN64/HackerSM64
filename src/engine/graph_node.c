122c591c4e7a73a862709eb859de07c7a85df9d0
Arceveti <arceveti@gmail.com>
2022-05-16 21:15:21 -0700

    Add missing check for 9 vert shadows in LEGACY_SHADOW_IDS ifdef

--- src/engine/graph_node.c
+++ src/engine/graph_node.c
@@ -430,7 +430,7 @@ struct GraphNodeShadow *init_graph_node_shadow(struct AllocOnlyPool *pool,
 
     if (graphNode != NULL) {
 #ifdef LEGACY_SHADOW_IDS
-        if (shadowType == 1 || shadowType == 2 || shadowType == 99) {
+        if (shadowType == 0 || shadowType == 1 || shadowType == 2 || shadowType == 99) {
             shadowType = SHADOW_CIRCLE;
         } else if (shadowType == 11 || shadowType == 12) {
             shadowType = SHADOW_SQUARE;
