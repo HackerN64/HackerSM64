5276d43afe5301a62d35eda1dcff51fe840277d8
Reonu <danileon95@gmail.com>
2022-05-25 16:23:13 +0100

    moved the defines to hud.h

--- src/game/hud.c
+++ src/game/hud.c
@@ -26,18 +26,6 @@
  * cannon reticle, and the unused keys.
  **/
 
-#define HUD_POWER_METER_X            140
-#define HUD_POWER_METER_EMPHASIZED_Y 166
-#define HUD_POWER_METER_Y            200
-#define HUD_POWER_METER_HIDDEN_Y     300
-
-#define HUD_TOP_Y 209
-#define HUD_BOTTOM_Y 19 // Same height as the Lakitu icon
-
-#define HUD_COINS_X 168
-#define HUD_STARS_X 78 // This starts counting from the right edge
-#define HUD_CAMERA_X 54 // This starts counting from the right edge
-
 #ifdef BREATH_METER
 // #ifdef DISABLE_LIVES
 // #define HUD_BREATH_METER_X         64
