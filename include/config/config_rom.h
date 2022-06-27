bd17eb432beaf185bbf1cee02579c0ff827026f0
Reonu <danileon95@gmail.com>
2022-06-27 19:19:04 +0100

    newline moment

--- include/config/config_rom.h
+++ include/config/config_rom.h
@@ -29,6 +29,7 @@
 #define BORDER_HEIGHT_CONSOLE  0
 #define BORDER_HEIGHT_EMULATOR 0
 
+<<<<<<< HEAD
 /**
  * Informs supported emulators to default to gamecube controller inputs.
  */
@@ -38,4 +39,8 @@
  * RCVI hack. Increases performance on emulator, and does nothing on console.
  * Might break on some emulators. Use at your own risk, and don't use it unless you actually need the extra performance.
  */
+=======
+// RCVI hack. Increases performance on emulator, and does nothing on console
+// Might break on some emulators. Use at your own risk, and don't use it unless you actually need the extra performance.
+>>>>>>> newline moment
 // #define RCVI_HACK
