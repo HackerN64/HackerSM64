f7c3f670658cf2e2384429ad1ef221c6367d0582
gheskett <gheskett@gmail.com>
2022-09-30 22:40:27 -0400

    Remove nonexistent variable and clean up

--- src/game/mario_actions_moving.c
+++ src/game/mario_actions_moving.c
@@ -434,10 +434,12 @@ void update_walking_speed(struct MarioState *m) {
     if (m->forwardVel <= 0.0f) {
         m->forwardVel += 1.1f;
     } else if (m->forwardVel <= targetSpeed) {
-        if (m->forwardVel <= 8.f) {
-            m->forwardVel = MIN(m->intendedMag, 8.f); // same fix as melee dashback
+        // Slow down if moving backwards
+        if (m->forwardVel <= 8.0f) {
+            m->forwardVel = MIN(m->intendedMag, 8.0f); // same fix as melee dashback
         }
-        m->forwardVel += (1.1f - m->forwardVel / dividor);
+        // If accelerating
+        m->forwardVel += 1.1f - m->forwardVel / 43.0f;
     } else if (m->floor->normal.y >= 0.95f) {
         m->forwardVel -= 1.0f;
     }
