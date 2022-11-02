881a7e413237d7323e8fdcf8c9ca885c99b858db
Reonu <danileon95@gmail.com>
2022-11-02 22:37:54 +0100

    grab force from the second byte

--- src/game/level_update.c
+++ src/game/level_update.c
@@ -744,7 +744,7 @@ s16 level_trigger_warp(struct MarioState *m, s32 warpOp) {
 
             case WARP_OP_WARP_FLOOR:
                 if (m->floor->type == SURFACE_WARP && (m->floor->force != 0x00)) {
-                    sSourceWarpNodeId = m->floor->force;
+                    sSourceWarpNodeId = m->floor->force & 0xFF;
                 } else {
                     sSourceWarpNodeId = WARP_NODE_WARP_FLOOR;
                     if (area_get_warp_node(sSourceWarpNodeId) == NULL) {
