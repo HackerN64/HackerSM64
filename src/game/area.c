d8daaf63437a934a745281186a586067d6125913
Arceveti <arceveti@gmail.com>
2022-12-05 00:22:06 -0500

    Act-specific whirlpools + fix act 6 objects

--- src/game/area.c
+++ src/game/area.c
@@ -177,7 +177,7 @@ void load_obj_warp_nodes(void) {
 }
 
 void clear_areas(void) {
-    s32 i;
+    s32 i, j;
 
     gCurrentArea = NULL;
     gWarpTransition.isActive = FALSE;
@@ -198,8 +198,9 @@ void clear_areas(void) {
         gAreaData[i].objectSpawnInfos = NULL;
         gAreaData[i].camera = NULL;
         gAreaData[i].unused = NULL;
-        gAreaData[i].whirlpools[0] = NULL;
-        gAreaData[i].whirlpools[1] = NULL;
+        for (j = 0; j < ARRAY_COUNT(gAreaData[i].whirlpools); j++) {
+            gAreaData[i].whirlpools[j] = NULL;
+        }
         gAreaData[i].dialog[0] = DIALOG_NONE;
         gAreaData[i].dialog[1] = DIALOG_NONE;
         gAreaData[i].musicParam = 0;
