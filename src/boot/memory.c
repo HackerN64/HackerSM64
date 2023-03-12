4b237fd24471187c71617460d82d3c7e0a291dc7
Fazana <52551480+FazanaJ@users.noreply.github.com>
2023-03-12 16:29:08 +0000

    2.1.0 Puppyprint Changes (#362)
    * Puppyprint text size
    
    * Update puppyprint.c
    
    * Revert broken render_multi_image changes
    
    * revert multi image tweak
    
    parallel momento
    
    * Update puppyprint debug colors to work with new hexadecimal system
    
    * Puppyprint color reversion to currEnv + some bugfixes
    
    * Remove a pointless color array
    
    * Add forgotten comment
    
    * i can type yes definitely
    
    * Fix some puppyprint formatting edge cases
    
    * Missed one last puppyprint edge case
    
    * Additional strLen check
    
    * Variable length fix + command ignoring strlen
    
    * type changes
    
    * Update puppyprint.c
    
    * Box clamping
    
    * Revamped profiling, fixed right alignment
    
    * Update puppycam2.c
    
    * Deferred printing
    
    * Update puppyprint.c
    
    * Fixed negatives being stupid
    
    * Puppyprint Memory view rework
    
    * Update level_script.c
    
    * tweaks
    
    * Fix new line spacing with different scales
    
    also small bit of optimisation
    
    * Spacing fixes
    
    * Lightweight text option
    
    * puppyprint debug now using light text where applicable
    
    * Update puppyprint.c
    
    * Update puppyprint.c
    
    * level select menu
    
    * merge profilers into one WIP
    
    * Fully ascii compliant
    
    * Finishing touches hopefully
    
    * Update puppyprint.c
    
    * new font
    
    * vanilla font
    
    * o
    
    * Update hud.c
    
    * last kerning fixes
    
    * Requested changes
    
    * requested changes
    
    * fix the j
    
    * requested changes
    
    * change ur clothes
    
    * Update surface_collision.c
    
    * requested changes
    
    * Update puppyprint.h
    
    * Update camera.c
    
    * .
    
    * Update puppyprint.c
    
    * Update puppyprint.c
    
    * Update puppyprint.c
    
    * fix J
    
    * font
    
    * font system rework
    
    * ia4 outline font
    
    * pain
    
    * fix deferred printing
    
    * fix format change
    
    * Add audio profiling to Puppyprint Debug
    
    * Optimize audio profiling substantially
    
    * Minor audio optimizations
    
    * requested changes
    
    * oops accidentally commited 6 lmfao
    
    * Update game_init.c
    
    * Update puppyprint.c
    
    * Update puppyprint.c
    
    * Improve Puppyprint deferred prints
    
    This is safer, more readable, and more runtime efficient all in the same package
    
    * change return type
    
    * fix building with profiler only
    
    * Update profiling.c
    
    * fix upscaled texture rectangles from multi image
    
    ---------
    
    Co-authored-by: gheskett <gheskett@gmail.com>

--- src/boot/memory.c
+++ src/boot/memory.c
@@ -130,7 +130,7 @@ void main_pool_init(void *start, void *end) {
     sPoolListHeadL->next = NULL;
     sPoolListHeadR->prev = NULL;
     sPoolListHeadR->next = NULL;
-#if PUPPYPRINT_DEBUG
+#ifdef PUPPYPRINT_DEBUG
     mempool = sPoolFreeSpace;
 #endif
 }
@@ -335,8 +335,9 @@ void *load_segment(s32 segment, u8 *srcStart, u8 *srcEnd, u32 side, u8 *bssStart
             set_segment_base_addr(segment, addr);
         }
     }
-#if PUPPYPRINT_DEBUG
-    ramsizeSegment[(segment + nameTable) - 2] = ((s32)srcEnd - (s32)srcStart);
+#ifdef PUPPYPRINT_DEBUG
+    u32 ppSize = ALIGN16(srcEnd - srcStart) + 16;
+    set_segment_memory_printout(segment, ppSize);
 #endif
     return addr;
 }
@@ -412,8 +413,9 @@ void *load_segment_decompress(s32 segment, u8 *srcStart, u8 *srcEnd) {
             main_pool_free(compressed);
         }
     }
-#if PUPPYPRINT_DEBUG
-    ramsizeSegment[(segment + nameTable) - 2] = (s32)srcEnd - (s32)srcStart;
+#ifdef PUPPYPRINT_DEBUG
+    u32 ppSize = ALIGN16((u32)*size) + 16;
+    set_segment_memory_printout(segment, ppSize);
 #endif
     return dest;
 }
@@ -544,6 +546,9 @@ struct MemoryPool *mem_pool_init(u32 size, u32 side) {
         block->next = NULL;
         block->size = pool->totalSpace;
     }
+#ifdef PUPPYPRINT_DEBUG
+    gPoolMem += ALIGN16(size) + 16;
+#endif
     return pool;
 }
 
--- src/engine/level_script.c
+++ src/engine/level_script.c
@@ -347,6 +347,9 @@ void unmap_tlbs(void) {
                     osUnmapTLB(gTlbEntries);
                     gTlbSegments[i]--;
                     gTlbEntries--;
+#ifdef PUPPYPRINT_DEBUG
+                    set_segment_memory_printout(i, 0);
+#endif
                 }
             } else {
                 gTlbEntries -= gTlbSegments[i];
@@ -830,7 +833,7 @@ static void level_cmd_puppyvolume(void) {
     if ((sPuppyVolumeStack[gPuppyVolumeCount] = mem_pool_alloc(gPuppyMemoryPool, sizeof(struct sPuppyVolume))) == NULL) {
         sCurrentCmd = CMD_NEXT;
         gPuppyError |= PUPPY_ERROR_POOL_FULL;
-#if PUPPYPRINT_DEBUG
+#ifdef PUPPYPRINT_DEBUG
         append_puppyprint_log("Puppycamera volume allocation failed.");
 #endif
         return;
@@ -880,7 +883,7 @@ static void level_cmd_puppylight_node(void) {
 #ifdef PUPPYLIGHTS
     gPuppyLights[gNumLights] = mem_pool_alloc(gLightsPool, sizeof(struct PuppyLight));
     if (gPuppyLights[gNumLights] == NULL) {
-#if PUPPYPRINT_DEBUG
+#ifdef PUPPYPRINT_DEBUG
         append_puppyprint_log("Puppylight allocation failed.");
 #endif
         sCurrentCmd = CMD_NEXT;
