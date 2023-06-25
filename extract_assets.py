d2f7b4ca3a55ea586a0d63ac2f335e429ee8a93e
someone2639 <someone2639@gmail.com>
2023-06-25 16:06:22 -0400

    extract_assets can now find a baserom named anything

--- extract_assets.py
+++ extract_assets.py
@@ -3,12 +3,41 @@ import sys
 import os
 import json
 
+
 sha1_LUT = {
     "eu": "4ac5721683d0e0b6bbb561b58a71740845dceea9",
     "jp": "8a20a5c83d6ceb0f0506cfc9fa20d8f438cafe51",
     "sh": "3f319ae697533a255a1003d09202379d78d5a2e0",
     "us": "9bef1128717f958171a4afac3ed78ee2bb4e86ce",
 }
+
+sha1_swapLUT = {
+    "eu": "d80ee9eeb6454d53a96ceb6ed0aca3ffde045091",
+    "jp": "1d2579dd5fb1d8263a4bcc063a651a64acc88921",
+    "sh": "2a2b85e94581545ca3c05b8f864b488b141a8a1f",
+    "us": "1002dd7b56aa0a59a9103f1fb3d57d6b161f8da7",
+}
+
+def get_files():
+    import subprocess
+    fileArray = [f for f in os.listdir(os.getcwd()) if os.path.isfile(f)]
+    foundVersions = {}
+
+    for f in fileArray:
+        p = subprocess.Popen(
+            ["sha1sum", f],
+            stdout=subprocess.PIPE
+        )
+        sha1sum = p.communicate()[0].decode('ascii').split()[0]
+        for k, v in sha1_LUT.items():
+            if v == sha1sum:
+                foundVersions[k] = f
+
+        for k, v in sha1_swapLUT.items():
+            if v == sha1sum:
+                foundVersions[k] = f
+    return foundVersions
+
 
 def read_asset_map():
     with open("assets.json") as f:
@@ -68,6 +97,8 @@ def main():
     # In case we ever need to change formats of generated files, we keep a
     # revision ID in the local asset file.
     new_version = 7
+
+    fileLUT = get_files()
 
     try:
         local_asset_file = open(".assets-local.txt")
@@ -140,7 +171,7 @@ def main():
     # Load ROMs
     roms = {}
     for lang in langs:
-        fname = "baserom." + lang + ".z64"
+        fname = fileLUT[lang]
         try:
             with open(fname, "rb") as f:
                 roms[lang] = f.read()
@@ -149,15 +180,25 @@ def main():
             sys.exit(1)
         sha1 = hashlib.sha1(roms[lang]).hexdigest()
         expected_sha1 = sha1_LUT[lang]
+        swapped_sha1 = sha1_LUT[lang]
         if sha1 != expected_sha1:
-            print(
-                fname
-                + " has the wrong hash! Found "
-                + sha1
-                + ", expected "
-                + expected_sha1
-            )
-            sys.exit(1)
+            if sha1 == swapped_sha1:
+                subprocess.run(
+                    [
+                        "dd","conv=swab",
+                        "if=%s" % fname,
+                        "of=/tmp/hackersm64_swapped.z64"
+                    ]
+                )
+            else:
+                print(
+                    fname
+                    + " has the wrong hash! Found "
+                    + sha1
+                    + ", expected "
+                    + expected_sha1
+                )
+                sys.exit(1)
 
     # Make sure tools exist
     subprocess.check_call(
