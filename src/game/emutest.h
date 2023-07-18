d2cc8aba7be26eaa2501db473e9949afa82a4b17
Matt Pharoah <mtpharoah@gmail.com>
2023-07-18 11:46:47 -0400

    Updated enum values so you can safely AND and OR all emulator version flags together

--- src/game/emutest.h
+++ src/game/emutest.h
@@ -2,21 +2,19 @@
 #define EMUTEST_H
 
 enum Emulator {
-    EMU_WIIVC = 0x0100,
-    EMU_PROJECT64_ANY = 0x0200,
-        EMU_PROJECT64_1_OR_2 = 0x0201, // PJ64 1.6 or similar (up to 2.3)
-        EMU_PROJECT64_3 = 0x0204, // PJ64 3.X
-        EMU_PROJECT64_4 = 0x0208, // PJ64 4.0 or later
-    EMU_MUPEN_BASED = 0x0400, // mupen64plus or pre-2.12 paralleln64, but NOT simple64 or new paralleln64
-        EMU_MUPEN_OLD = 0x0401, // 1964 and pre-2.12 paralleln64 will also get detected as this
-        EMU_MUPEN64PLUS_NEXT = 0x0402,
-    EMU_CEN64 = 0x0800,
-    EMU_SIMPLE64 = 0x1000,
-    EMU_PARALLELN64 = 0x2000, // ParallelN64 2.12 or later
-    EMU_ARES = 0x4000,
-    EMU_CONSOLE = 0x8000,
-
-    EMU_MASK = 0xFF00
+    EMU_WIIVC = 0x0001,
+    EMU_PROJECT64_ANY = 0x001E,
+        EMU_PROJECT64_1_OR_2 = 0x0006, // PJ64 1.6 or similar (up to 2.3)
+        EMU_PROJECT64_3 = 0x0008, // PJ64 3.X
+        EMU_PROJECT64_4 = 0x0010, // PJ64 4.0 or later
+    EMU_MUPEN_BASED = 0x0060, // mupen64plus or pre-2.12 paralleln64, but NOT simple64 or new paralleln64
+        EMU_MUPEN_OLD = 0x0020, // 1964 and pre-2.12 paralleln64 will also get detected as this
+        EMU_MUPEN64PLUS_NEXT = 0x040,
+    EMU_CEN64 = 0x0080,
+    EMU_SIMPLE64 = 0x0100,
+    EMU_PARALLELN64 = 0x0200, // ParallelN64 2.12 or later
+    EMU_ARES = 0x0400,
+    EMU_CONSOLE = 0x0800
 };
 
 // initializes gEmulator, gIsConsole, gIsVC, and gCacheEmulated
@@ -24,7 +22,7 @@ extern void detect_emulator();
 
 /* gEmulator is an enum that identifies the current emulator.
  * The enum values work as a bitfield, so you can use the & and | operators
- * to test for multiple emulators at once.
+ * to test for multiple emulators or versions at once.
  * 
  * Examples:
  * 
@@ -32,7 +30,7 @@ extern void detect_emulator();
  * if (gEmulator & EMU_PROJECT64_ANY)
  * 
  * Test for only PJ64 < 3.0:
- * if (gEmulator == EMU_PROJECT64_1_OR_2) // don't use & with specific versions
+ * if (gEmulator & EMU_PROJECT64_1_OR_2)
  * 
  * Test for Console, Ares, or ParallelN64:
  * if (gEmulator & (EMU_CONSOLE | EMU_ARES | EMU_PARALLELN64))
