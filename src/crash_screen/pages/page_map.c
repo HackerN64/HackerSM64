05a60aec882c66a777e4d22234497e6507fdbc0c
Arceveti <arceveti@gmail.com>
2024-03-15 03:51:16 -0700

    Improve some setting names

--- src/crash_screen/pages/page_map.c
+++ src/crash_screen/pages/page_map.c
@@ -27,9 +27,9 @@
 
 struct CSSetting cs_settings_group_page_map[] = {
     [CS_OPT_HEADER_PAGE_MAP         ] = { .type = CS_OPT_TYPE_HEADER,  .name = "MAP VIEW",                       .valNames = &gValNames_bool,          .val = SECTION_EXPANDED_DEFAULT,  .defaultVal = SECTION_EXPANDED_DEFAULT,  .lowerBound = FALSE,                 .upperBound = TRUE,                       },
-    [CS_OPT_MAP_SHOW_ADDRESSES      ] = { .type = CS_OPT_TYPE_SETTING, .name = "Show addresses",                 .valNames = &gValNames_bool,          .val = TRUE,                      .defaultVal = TRUE,                      .lowerBound = FALSE,                 .upperBound = TRUE,                       },
-    [CS_OPT_MAP_SHOW_TYPES          ] = { .type = CS_OPT_TYPE_SETTING, .name = "Show types",                     .valNames = &gValNames_bool,          .val = TRUE,                      .defaultVal = TRUE,                      .lowerBound = FALSE,                 .upperBound = TRUE,                       },
-    [CS_OPT_MAP_SHOW_SIZES          ] = { .type = CS_OPT_TYPE_SETTING, .name = "Show sizes",                     .valNames = &gValNames_bool,          .val = TRUE,                      .defaultVal = TRUE,                      .lowerBound = FALSE,                 .upperBound = TRUE,                       },
+    [CS_OPT_MAP_SHOW_ADDRESSES      ] = { .type = CS_OPT_TYPE_SETTING, .name = "Show symbol addresses",          .valNames = &gValNames_bool,          .val = TRUE,                      .defaultVal = TRUE,                      .lowerBound = FALSE,                 .upperBound = TRUE,                       },
+    [CS_OPT_MAP_SHOW_TYPES          ] = { .type = CS_OPT_TYPE_SETTING, .name = "Show symbol types",              .valNames = &gValNames_bool,          .val = TRUE,                      .defaultVal = TRUE,                      .lowerBound = FALSE,                 .upperBound = TRUE,                       },
+    [CS_OPT_MAP_SHOW_SIZES          ] = { .type = CS_OPT_TYPE_SETTING, .name = "Show symbol sizes",              .valNames = &gValNames_bool,          .val = TRUE,                      .defaultVal = TRUE,                      .lowerBound = FALSE,                 .upperBound = TRUE,                       },
     [CS_OPT_END_MAP                 ] = { .type = CS_OPT_TYPE_END, },
 };
 
