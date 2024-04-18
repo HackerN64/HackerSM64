104fde499086efb73bdf22e69b3855f4ec41f9a5
someone2639 <someone2639@gmail.com>
2024-04-17 22:19:23 -0400

    Add compatibility defines for script functions in case Fast64 hates this change

--- levels/scripts.h
+++ levels/scripts.h
@@ -6,23 +6,42 @@
 
 // scripts
 extern const LevelScript level_main_scripts_entry[];
+#define common1_load_geolayouts script_func_global_1
 extern const LevelScript script_func_global_1[];
+#define common0_load_geolayouts script_func_global_1
 extern const LevelScript script_func_global_2[];
+#define group1_load_geolayouts script_func_global_2
 extern const LevelScript script_func_global_3[];
+#define group2_load_geolayouts script_func_global_3
 extern const LevelScript script_func_global_4[];
+#define group3_load_geolayouts script_func_global_4
 extern const LevelScript script_func_global_5[];
+#define group4_load_geolayouts script_func_global_5
 extern const LevelScript script_func_global_6[];
+#define group5_load_geolayouts script_func_global_6
 extern const LevelScript script_func_global_7[];
+#define group6_load_geolayouts script_func_global_7
 extern const LevelScript script_func_global_8[];
+#define group7_load_geolayouts script_func_global_8
 extern const LevelScript script_func_global_9[];
+#define group8_load_geolayouts script_func_global_9
 extern const LevelScript script_func_global_10[];
+#define group9_load_geolayouts script_func_global_10
 extern const LevelScript script_func_global_11[];
+#define group10_load_geolayouts script_func_global_11
 extern const LevelScript script_func_global_12[];
+#define group11_load_geolayouts script_func_global_12
 extern const LevelScript script_func_global_13[];
+#define group12_load_geolayouts script_func_global_13
 extern const LevelScript script_func_global_14[];
+#define group13_load_geolayouts script_func_global_14
 extern const LevelScript script_func_global_15[];
+#define group14_load_geolayouts script_func_global_15
 extern const LevelScript script_func_global_16[];
+#define group15_load_geolayouts script_func_global_16
 extern const LevelScript script_func_global_17[];
+#define group16_load_geolayouts script_func_global_17
 extern const LevelScript script_func_global_18[];
+#define group17_load_geolayouts script_func_global_18
 
 #endif
