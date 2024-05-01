45a74a34f77c958a260ac3b87fc6138050b7a527
Arceveti <arceveti@gmail.com>
2024-05-01 16:07:28 -0700

    Move attributes to attributes.h

--- include/macros.h
+++ include/macros.h
@@ -7,6 +7,7 @@
 #define GLOBAL_ASM(...)
 #endif
 
+#include "attributes.h"
 #include "builtins.h"
 
 #define member(_T, _m) ((_T*)0)->_m
@@ -41,104 +42,6 @@
         ".previous\n" \
     ); \
     extern _type _name[];
-
-// Avoid compiler warnings for unused variables.
-#ifdef __GNUC__
-#define UNUSED __attribute__((unused))
-#else
-#define UNUSED
-#endif
-
-// Avoid undefined behaviour for non-returning functions.
-#ifdef __GNUC__
-#define NORETURN __attribute__((noreturn))
-#else
-#define NORETURN
-#endif
-
-// Always inline a function.
-#ifdef __GNUC__
-#define ALWAYS_INLINE inline __attribute__((always_inline))
-#else
-#define ALWAYS_INLINE inline
-#endif
-
-// Never inline a function.
-#ifdef __GNUC__
-#define NEVER_INLINE __attribute__((noinline))
-#else
-#define NEVER_INLINE
-#endif
-
-// Fall through a switch case.
-#ifdef __GNUC__
-#define FALL_THROUGH __attribute__((fallthrough))
-#else
-#define FALL_THROUGH
-#endif
-
-// Use Og when compiling the function.
-#ifdef __GNUC__
-#define OPTIMIZE_OG __attribute__((optimize("Og")))
-#else
-#define OPTIMIZE_OG
-#endif
-
-// Use Os when compiling the function.
-#ifdef __GNUC__
-#define OPTIMIZE_OS __attribute__((optimize("Os")))
-#else
-#define OPTIMIZE_OS
-#endif
-
-// Use Ofast when compiling the function.
-#ifdef __GNUC__
-#define OPTIMIZE_OFAST __attribute__((optimize("Ofast")))
-#else
-#define OPTIMIZE_OFAST
-#endif
-
-// Ignore 4-byte alignment in structs.
-#ifdef __GNUC__
-#define PACKED __attribute__((packed))
-#else
-#define PACKED
-#endif
-
-// Align to 4-byte boundary.
-#ifdef __GNUC__
-#define ALIGNED4 __attribute__((aligned(4)))
-#else
-#define ALIGNED4
-#endif
-
-// Align to 8-byte boundary (for DMA requirements).
-#ifdef __GNUC__
-#define ALIGNED8 __attribute__((aligned(8)))
-#else
-#define ALIGNED8
-#endif
-
-// Align to 16-byte boundary (for audio lib requirements).
-#ifdef __GNUC__
-#define ALIGNED16 __attribute__((aligned(16)))
-#else
-#define ALIGNED16
-#endif
-
-// Align to 32-byte boundary.
-#ifdef __GNUC__
-#define ALIGNED32 __attribute__((aligned(32)))
-#else
-#define ALIGNED32
-#endif
-
-// Align to 64-byte boundary.
-#ifdef __GNUC__
-#define ALIGNED64 __attribute__((aligned(64)))
-#else
-#define ALIGNED64
-#endif
 
 // Align to previous boundry.
 #ifndef ALIGNFLOOR
