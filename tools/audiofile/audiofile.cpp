5ed6e272ba564d567b0b66947b562f99d857a24a
aglab2 <aglab3@gmail.com>
2024-07-03 08:35:28 +0800

    Revert "Fixed audiofile warnings (#811)"
    This reverts commit 500507509ba0ae8b49afd39286e8fb650b797e96.

--- tools/audiofile/audiofile.cpp
+++ tools/audiofile/audiofile.cpp
@@ -3562,12 +3562,12 @@ struct signConverter
 	static const int kMaxSignedValue = (((1 << (kScaleBits - 1)) - 1) << 1) + 1;
 	static const int kMinSignedValue = -kMaxSignedValue - 1;
 
-	struct signedToUnsigned : public std::function<UnsignedType(SignedType)>
+	struct signedToUnsigned : public std::unary_function<SignedType, UnsignedType>
 	{
 		UnsignedType operator()(SignedType x) { return x - kMinSignedValue; }
 	};
 
-	struct unsignedToSigned : public std::function<UnsignedType(SignedType)>
+	struct unsignedToSigned : public std::unary_function<SignedType, UnsignedType>
 	{
 		SignedType operator()(UnsignedType x) { return x + kMinSignedValue; }
 	};
@@ -3760,7 +3760,7 @@ private:
 };
 
 template <typename Arg, typename Result>
-struct intToFloat : public std::function<Result(Arg)>
+struct intToFloat : public std::unary_function<Arg, Result>
 {
 	Result operator()(Arg x) const { return x; }
 };
@@ -3826,13 +3826,13 @@ private:
 };
 
 template <typename Arg, typename Result, unsigned shift>
-struct lshift : public std::function<Result(Arg)>
+struct lshift : public std::unary_function<Arg, Result>
 {
 	Result operator()(const Arg &x) const { return x << shift; }
 };
 
 template <typename Arg, typename Result, unsigned shift>
-struct rshift : public std::function<Result(Arg)>
+struct rshift : public std::unary_function<Arg, Result>
 {
 	Result operator()(const Arg &x) const { return x >> shift; }
 };
@@ -3928,7 +3928,7 @@ private:
 };
 
 template <typename Arg, typename Result>
-struct floatToFloat : public std::function<Result(Arg)>
+struct floatToFloat : public std::unary_function<Arg, Result>
 {
 	Result operator()(Arg x) const { return x; }
 };
