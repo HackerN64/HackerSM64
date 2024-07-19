c0378be6d9c2882e62fd61e73a5bdd3cce981632
someone2639 <someone2639@gmail.com>
2024-07-18 23:25:55 -0400

    libcart is not here yet

--- src/libcart/ff/memset.c
+++
@@ -1,8 +0,0 @@
-#include <string.h>
-
-void *memset(void *s, int c, size_t n)
-{
-	char *dst = s;
-	while (n-- > 0) *dst++ = c;
-	return s;
-}
--- src/libcart/src/cicardinit.c
+++
@@ -1,8 +0,0 @@
-#include <cart.h>
-#include "cartint.h"
-#include "ci.h"
-
-int ci_card_init(void)
-{
-	return 0;
-}
