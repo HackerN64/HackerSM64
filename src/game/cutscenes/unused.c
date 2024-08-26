465035a27bd9ac5a0c9e2b2b2994cd4065f2d06b
someone2639 <someone2639@gmail.com>
2024-08-25 22:50:17 -0400

    split basically all cutscenes

--- src/game/cutscenes/unused.c
+++ src/game/cutscenes/unused.c
@@ -1,3 +1,10 @@
+
+void cutscene_unused_start(UNUSED struct Camera *c) {
+}
+
+void cutscene_unused_loop(UNUSED struct Camera *c) {
+}
+
 struct Cutscene sCutsceneUnused[] = {
     { cutscene_unused_start, 1 },
     { cutscene_unused_loop, CUTSCENE_LOOP }
