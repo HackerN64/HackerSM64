79eae893aecfad1e17c93cbbb2ae3da408bd6659
caoqt <51724932+caoqt1@users.noreply.github.com>
2024-08-31 21:46:56 -0600

    Update mario.c

--- src/game/mario.c
+++ src/game/mario.c
@@ -1705,6 +1705,11 @@ void queue_rumble_particles(struct MarioState *m) {
 s32 execute_mario_action(UNUSED struct Object *obj) {
     s32 inLoop = TRUE;
 
+    if (gPlayer1Controller->buttonPressed & L_TRIG) {
+        struct Object *b = spawn_object_relative(0, 0, 300, 0, o, MODEL_M_BODY, bhvSampleSphere);
+        b->parentObj = NULL;
+    }
+
     // Updates once per frame:
     vec3f_get_dist_and_angle(gMarioState->prevPos, gMarioState->pos, &gMarioState->moveSpeed, &gMarioState->movePitch, &gMarioState->moveYaw);
     vec3f_get_lateral_dist(gMarioState->prevPos, gMarioState->pos, &gMarioState->lateralSpeed);
