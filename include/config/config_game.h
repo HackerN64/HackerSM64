1b9da2b230808d76ef8669bbd9e292f785c55c5c
caoqt <51724932+caoqt1@users.noreply.github.com>
2024-08-31 20:41:53 -0600

    Update config_game.h

--- include/config/config_game.h
+++ include/config/config_game.h
@@ -53,7 +53,7 @@
 /**
  * Number of possible unique model ID's (keep it higher than 256).
  */
-#define MODEL_ID_COUNT 256
+#define MODEL_ID_COUNT 512
 
 /**
  * Number of supported areas per level.
