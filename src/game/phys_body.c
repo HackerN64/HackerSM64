7c58394f88d5b813163aa502d83109e3257cd2f6
caoqt <51724932+caoqt1@users.noreply.github.com>
2024-08-31 20:34:17 -0600

    Create phys_body.c

---
+++ src/game/phys_body.c
@@ -0,0 +1,20 @@
+#include "src/game/phys_body.h"
+
+Vec3f sampleCube[8] = {
+    {100, 100, 100},
+    {100, 100, -100},
+    {-100, 100, -100},
+    {-100, 100, 100},
+    {100, -100, 100},
+    {100, -100, -100},
+    {-100, -100, -100},
+    {-100, -100, 100}
+};
+
+void run_object_physics_body(struct Object *obj) {
+
+}
+
+void init_object_physics_mesh(struct Object *obj) {
+
+}
