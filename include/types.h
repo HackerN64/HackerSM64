d1d337d3235bcabe422e8b068ef8e512234e4208
caoqt <51724932+caoqt1@users.noreply.github.com>
2024-08-31 20:49:42 -0600

    Update types.h

--- include/types.h
+++ include/types.h
@@ -300,7 +300,7 @@ struct Object {
         const void *asConstVoidPtr[MAX_OBJECT_FIELDS];
     } ptrData;
 #endif
-    /*0x1C8*/ u32 unused1;
+    /*0x1C8*/ struct RigidBody *rigidBody;
     /*0x1CC*/ const BehaviorScript *curBhvCommand;
     /*0x1D0*/ u32 bhvStackIndex;
     /*0x1D4*/ uintptr_t bhvStack[8];
