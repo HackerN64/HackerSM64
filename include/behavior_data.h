50a6aad9ca1ba36475798648990d56e9cfbdc2d7
caoqt <51724932+caoqt1@users.noreply.github.com>
2024-08-31 21:44:24 -0600

    Update behavior_data.h

--- include/behavior_data.h
+++ include/behavior_data.h
@@ -542,3 +542,6 @@ extern const BehaviorScript bhvIntroScene[];
 extern const BehaviorScript bhvUnusedFakeStar[];
 
 #endif // BEHAVIOR_DATA_H
+
+extern const BehaviorScript bhvSampleCube[];
+extern const BehaviorScript bhvSampleSphere[];
