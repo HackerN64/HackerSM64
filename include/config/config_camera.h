24743f69058474e7c4a466743e0d9c851d837309
Reonu <danileon95@gmail.com>
2025-02-13 16:13:12 +0000

    use block comment

--- include/config/config_camera.h
+++ include/config/config_camera.h
@@ -50,9 +50,10 @@
  * Enables "parallel lakitu camera" or "aglab cam" which lets you move the camera smoothly with the D-pad. Will be disabled if Reonucam is enabled.
  */
 #define PARALLEL_LAKITU_CAM
-
-// Enables Reonucam, a custom camera that aims to be a more feature-rich "aglabcam" that doesn't use a single button more than the vanilla camera. 
-// An explanation the features can be seen here: https://www.youtube.com/watch?v=TQNkznX9Z3k (please note that the analog feature shown at the end is no longer present)
+/**
+ * Enables Reonucam, a custom camera that aims to be a more feature-rich "aglabcam" that doesn't use a single button more than the vanilla camera. 
+ * An explanation the features can be seen here: https://www.youtube.com/watch?v=TQNkznX9Z3k (please note that the analog feature shown at the end is no longer present)
+ */
 // #define REONUCAM
 
 /**
