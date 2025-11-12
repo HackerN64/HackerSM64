136593c2061723ef5e572e7d41ee2825b2fbb3ac
someone2639 <someone2639@gmail.com>
2025-11-11 19:27:02 -0500

    tab ->

--- src/debugger/crash_screen.c
+++ src/debugger/crash_screen.c
@@ -731,7 +731,7 @@ void draw_crash_screen(OSThread *thread) {
             case CRASH_SCREEN_PAGE_SIMPLE:     draw_crash_overview(thread, cause); break;
             case CRASH_SCREEN_PAGE_CONTEXT:    draw_crash_context(thread, cause); break;
 #ifdef PUPPYPRINT_DEBUG
-            case CRASH_SCREEN_PAGE_LOG: 		  draw_crash_log(); break;
+            case CRASH_SCREEN_PAGE_LOG:        draw_crash_log(); break;
 #endif
             case CRASH_SCREEN_PAGE_STACKTRACE: draw_stacktrace(thread, cause); break;
             case CRASH_SCREEN_PAGE_DISASM:     draw_disasm(thread); break;
