a83aae80e9f06b8f4d4d3e81d96a85a3b17c4eaf
someone2639 <someone2639@gmail.com>
2025-11-14 20:31:14 -0500

    newline

--- tools/getGoddardSize.py
+++ tools/getGoddardSize.py
@@ -13,5 +13,5 @@ with open(preliminaryMapPath) as mapFile:
                 sz: int = int(tokens[0], 16)
                 sz += 16
                 sz &= 0xFFFFFFF0
-                outputFile.write(f"GODDARD_SIZE = 0x{sz:X};")
+                outputFile.write(f"GODDARD_SIZE = 0x{sz:X};\n")
                 sys.exit(0)
