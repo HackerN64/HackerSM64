3f67447eddd5b11a397788d6e8c5f2048dd5a880
lucretia <git@lucretia.sh>
2026-03-28 19:42:34 +1100

    Move last safe pos update to a more sensible place

--- src/game/mario.c
+++ src/game/mario.c
@@ -1754,6 +1754,12 @@ s32 execute_mario_action(UNUSED struct Object *obj) {
             return ACTIVE_PARTICLE_NONE;
         }
 
+        // H64 TODO: Add config opt & check if floor is slippery
+        u32 actGroup = gMarioState->action & ACT_GROUP_MASK;
+        if ((actGroup == ACT_GROUP_STATIONARY || actGroup == ACT_GROUP_MOVING) && !SURFACE_IS_UNSAFE(gMarioState->floor->type)) {
+            vec3f_copy(gMarioState->lastSafePos, gMarioState->pos);
+        }
+
         // The function can loop through many action shifts in one frame,
         // which can lead to unexpected sub-frame behavior. Could potentially hang
         // if a loop of actions were found, but there has not been a situation found.
