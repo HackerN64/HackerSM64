b18b544faaae9f2434d40f37978020839947ef78
someone2639 <someone2639@gmail.com>
2026-08-28 21:19:13 -0400

    revert debug experiment

--- src/debugger/crash_screen.c
+++ src/debugger/crash_screen.c
@@ -428,14 +428,12 @@ void draw_crash_overview(OSThread *thread, s32 cause) {
         crash_screen_println("Stack Trace: ");
         reset_text_color();
 
-        #define CRASH_SCREEN_FUNCTION_PRINT_LENGTH (20)
-
         // print current func
-        crash_screen_println("%08X: %s", tc->pc, crash_screen_ellide_string(info.func, CRASH_SCREEN_FUNCTION_PRINT_LENGTH));
+        crash_screen_println("%08X: %s", tc->pc, info.func);
         // print last func
         u32 ret_addr = tc->ra;
         symtable_info_t ra_info = get_symbol_info(ret_addr);
-        crash_screen_println("%08X: %s:%d", ret_addr, crash_screen_ellide_string(info.func, CRASH_SCREEN_FUNCTION_PRINT_LENGTH), ra_info.line);
+        crash_screen_println("%08X: %s:%d", ret_addr, info.func, ra_info.line);
         // print up to 3 more
         for (u32 i = 0; i < MIN(3, sCrashScreenStackTraceCount); i++) {
             crash_screen_println(get_stack_entry(i));
